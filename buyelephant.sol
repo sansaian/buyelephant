f8e7348719111eb4c9f3a5949240f4dd8fb1b0ee
atrantin <anton@mm94.ru>
2017-08-28 14:47:26 +0300

    initial comments

--- buyelephant.sol
+++ buyelephant.sol
@@ -1,16 +1,29 @@
 pragma solidity ^0.4.0;
 contract Main  {
 
+    /*/
+     *  Contract fields
+    /*/
     address public owner;
     address public buyer;
     address public seller;
     address public rosreestr;
     address public fds;
     address public dduorderContract;
-     function Main(){
+     
+    function Main(){
         owner = msg.sender;
     }
 
+    /*/
+     *  Public functions
+    /*/
+    // @dev assign roles to public keys
+    // @returns success flag
+    // @param _buyer role building buyer
+    // @param _seller role building seller
+    // @param _rosreestr role ROSREESTR
+    // @param _fds role Fond dolevogo stroitelstva :)
     function createUser(address _buyer,address _seller,address _rosreestr,address _fds )isOwner returns(bool){
         buyer = _buyer;
         seller = _seller;
@@ -19,12 +32,21 @@ contract Main  {
         return true;
     }
 
+    /*/
+     *  Public functions
+    /*/
+    // @dev registers DDU document inside the system
+    // @returns created ddu address contract
+    // @param _idObject Building identity
+    // @param _cost DDU price
     function createDdu(string _idObject,uint _cost)isOwner returns (address){
-
         dduorderContract = new DduOrder(_idObject, _cost,buyer,seller,rosreestr,fds);
         return dduorderContract;
     }
 
+    /*/
+     *  Modifiers
+    /*/
      modifier isOwner {
         if (msg.sender == owner)
         _;
@@ -33,6 +55,9 @@ contract Main  {
 
 contract DduOrder  {
 
+    /*/
+     *  Contract fields
+    /*/
     address public buyer;
     address public seller;
     address public rosreestr;
@@ -43,6 +68,9 @@ contract DduOrder  {
     uint public balanceFrost=0;
     uint public status=0;
 
+    /*/
+     *  Сonstructor
+    /*/
     function DduOrder(string _idObject,uint _cost,address _buyer,address _seller,address _rosreestr,address _fds){
         idObject = _idObject;
         cost = _cost;
@@ -53,6 +81,11 @@ contract DduOrder  {
 
     }
 
+    /*/
+     *  Public functions
+    /*/
+
+    // @dev deposit money to seller account
     function depositMoneySeller() isSeller returns (uint) {
         if(status != 0)
             throw;
@@ -63,6 +96,7 @@ contract DduOrder  {
         return status;
     }
 
+    // @dev request rosreestr to register the DDU document
     function requestAccepted() isRosreestr returns (uint) {
         if(status != 1)
             throw;
@@ -72,6 +106,8 @@ contract DduOrder  {
         return status;
     }
 
+    // @dev "freeze" money of seller inside FDS fund
+    // @param summ amount of money to hold
     function reserveMoney(uint summ) isFds returns (uint) {
         if(status != 2)
             throw;
@@ -80,7 +116,6 @@ contract DduOrder  {
             balanceFrost = summ;}
         return status;
     }
-
 
     function reserveInRosreestr() isRosreestr returns (uint)  {
         if(status != 3)
@@ -146,5 +181,4 @@ contract DduOrder  {
         if (msg.sender == fds)
         _;
     }
-
 }
